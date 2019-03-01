9ee0781fdca6f3dfedbcf46a83658fd0011c7925
StanislavJochman <s.jochman@gmail.com>
2019-03-01 21:01:38 +0100

    Update Atmega8_IO.h

--- src/Atmega8_IO.h
+++ src/Atmega8_IO.h
@@ -16,7 +16,7 @@ class Cing
     void RunMotor(String motor,int speed= 100,String mode = "digital");
     int ReadLightSensor(int sensor = 1,String mode = "digital");
     int ReadUltrasonicSensor();
-    int ReadShineSensor(int sensor = 0);
+    int ReadShineSensor();
     bool ReadButton();
     bool ReadButtonExternal();
     int ReadPotentiometer();
